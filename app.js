49bf50c5ac9abf4245f8cba3bc11a408ae9353f3
Living Result <budgetbulkfit@gmail.com>
2026-05-07 19:32:19 +0530

    domain integration

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'https://www.getlivingresult.in',
+  'https://getlivingresult.in',
   process.env.FRONTEND_URL || 'https://living-resultm.vercel.app'
 ];
 
